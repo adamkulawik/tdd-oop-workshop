5f15d25624bda6eb37de3d5279ac98a1282815cb
adamkulawik <adamkulawik@wp.pl>
2019-11-09 00:31:49 +0100

    wip

---
+++ src/main/java/pl/tdd/samples/solid/i/ChargingStation.java
@@ -0,0 +1,24 @@
+package pl.tdd.samples.solid.i;
+
+public class ChargingStation {
+
+    public void install() {
+
+    }
+
+    public void deinstall() {
+
+    }
+
+    public void startCharging() {
+
+    }
+
+    public void stopCharging() {
+
+    }
+
+    public void setChargingProfile() {
+        
+    }
+}
